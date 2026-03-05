2a93cf00b4181c96aeeb994e7c9b3d6786b0898e
jaronsmith2006 <jaron.smith2006@gmail.com>
2026-03-05 10:10:47 +0530

    pricing plan related changes

--- docs-site/.docusaurus/routes.js
+++ docs-site/.docusaurus/routes.js
@@ -3,51 +3,16 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/',
-    component: ComponentCreator('/', '271'),
+    component: ComponentCreator('/', 'ca2'),
     routes: [
       {
         path: '/',
-        component: ComponentCreator('/', 'd4d'),
+        component: ComponentCreator('/', 'f13'),
         routes: [
           {
             path: '/',
-            component: ComponentCreator('/', '2e0'),
+            component: ComponentCreator('/', '976'),
             routes: [
               {
                 path: '/changelog',
@@ -136,12 +101,6 @@ export default [
               {
                 path: '/integrations/shopify',
                 component: ComponentCreator('/integrations/shopify', '335'),
-                exact: true,
-                sidebar: "merchantSidebar"
-              },
-              {
-                path: '/integrations/whatsapp',
-                component: ComponentCreator('/integrations/whatsapp', 'ce5'),
                 exact: true,
                 sidebar: "merchantSidebar"
               },
